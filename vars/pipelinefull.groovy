eafbdeec2b9adf1201a63e27b5722413ca824f71
Ricardo Sanchez <rjsmweb@gmail.com>
2022-03-17 16:57:09 -0300

    inicio parametrizacion

--- vars/pipelinefull.groovy
+++ vars/pipelinefull.groovy
@@ -1,10 +1,13 @@
 def call(){
 	pipeline {
+		options {
+			timeout(time: 30,unit: 'MINUTES')
+		}
 	agent any
 	stages {
 		stage('hola'){
 			steps{
-				println 'hola'
+				echo 'hola'
 			}
 
 		}
