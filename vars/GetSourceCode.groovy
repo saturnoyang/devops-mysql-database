16def4e7f2915a5feb6743b1d1a4a324ecf57d4f
Ricardo Sanchez <rjsmweb@gmail.com>
2022-03-31 14:44:30 -0300

    pruebas de integracion

--- vars/GetSourceCode.groovy
+++ vars/GetSourceCode.groovy
@@ -6,7 +6,7 @@ def call(String repo_url = 'not_defined',String repo_nombre = 'not_defined') {
 
   
   echo "Hello, ${repo_url}."
-  File f = new File("${repo_nombre}").exists();
+  File f = new File("${repo_nombre}");
   if(f.exists() && f.isDirectory()){
     echo "se debe eliminar el directorio"
   }
