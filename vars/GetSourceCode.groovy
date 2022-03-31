41b96b29f3f8b791f5ab325fb04f3811d7aa8dca
Ricardo Sanchez <rjsmweb@gmail.com>
2022-03-31 15:27:25 -0300

    pruebas de integracion

--- vars/GetSourceCode.groovy
+++ vars/GetSourceCode.groovy
@@ -4,17 +4,19 @@ def call(String repo_url = 'not_defined',String repo_nombre = 'not_defined') {
 
 
   def workspace = pwd()
+  def repo_ruta = pwd() + File.pathSeparator + repo_nombre
   
   echo "Hello, ${repo_url}."
-  File f = new File("${repo_nombre}");
+  
+  File f = new File("${repo_ruta}");
   if(f.exists() && f.isDirectory()){
     echo "se debe eliminar el directorio"
-    shortCommit = sh(returnStdout: true, script: "rm -fr $repo_nombre").trim()
+    shortCommit = sh(returnStdout: true, script: "rm -fr $repo_ruta").trim()
 
   }
   echo "la ruta deberia ser ${workspace}"
 
-  echo f.absolutePath
+
   shortCommit = sh(returnStdout: true, script: "git clone $repo_url").trim()
   
   
