924e33975a35d2094416a221b1e583328598ad04
Ricardo Sanchez <rjsmweb@gmail.com>
2022-03-31 16:11:11 -0300

    pruebas de integracion

--- vars/GetDatabase.groovy
+++ vars/GetDatabase.groovy
@@ -1,14 +1,17 @@
 #!/usr/bin/env groovy
-import groovy.yaml.YamlSlurper
+@Grab('org.yaml:snakeyaml:1.17')
+    
+import org.yaml.snakeyaml.Yaml
+
 
 def call(String repo_nombre = 'not_defined') {
 
 	def repo_ruta = pwd() + File.separator + repo_nombre
 	def database = repo_ruta + File.separator + "database.yaml"
 
-    
-    List example = new YamlSlurper().parseText(database)
-    
+    Yaml parser = new Yaml()
+    List example = parser.load((database as File).text)
+       
     example.each{println it.database}
 
 }
