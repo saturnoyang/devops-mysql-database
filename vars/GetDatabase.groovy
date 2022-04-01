c9813e600c0d7c6a3b9e598727583f98bbf5dfdf
Ricardo Sanchez <rjsmweb@gmail.com>
2022-04-01 11:05:34 -0300

    pruebas de integracion

--- vars/GetDatabase.groovy
+++ vars/GetDatabase.groovy
@@ -5,7 +5,7 @@ def call(String repo_nombre = 'not_defined') {
 	def repo_ruta = pwd() + File.separator + repo_nombre
 	def database = repo_ruta + File.separator + "database.yaml"
 
-	def info = readYaml (file: database)
+	def info = readYaml (file: "${database}")
       
     info.each{println it.database}
 
