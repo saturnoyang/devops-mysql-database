d525b2260a6453e45d3734bff89671ae77c4d364
Ricardo Sanchez <rjsmweb@gmail.com>
2022-04-01 16:53:48 -0300

    pruebas de integracion

--- vars/RunDatabaseScripts.groovy
+++ vars/RunDatabaseScripts.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
 
 def call(java.util.LinkedHashMap db_details ) {
-	echo db_details.url
-	echo db_details.puerto
-	echo db_details.usuario
-	echo db_details.password
+	echo db_details."url"
+	echo db_details."puerto"
+	echo db_details."usuario"
+	echo db_details."password"
 }
