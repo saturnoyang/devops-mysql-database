b3e1ce9585ee9068e08c8190381df2a40bdf6c9b
Ricardo Sanchez <rjsmweb@gmail.com>
2022-04-01 16:50:48 -0300

    pruebas de integracion

--- vars/RunDatabaseScripts.groovy
+++ vars/RunDatabaseScripts.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String db_details = 'not_defined') {
+def call(java.util.LinkedHashMap db_details ) {
 	echo db_details.url
 	echo db_details.puerto
 	echo db_details.usuario
