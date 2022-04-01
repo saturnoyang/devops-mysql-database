fa4ce3ab6c5e2c45cc87666ad67c15a3be64ba13
Ricardo Sanchez <rjsmweb@gmail.com>
2022-04-01 15:31:49 -0300

    pruebas de integracion

--- vars/GetDatabaseDetails.groovy
+++ vars/GetDatabaseDetails.groovy
@@ -3,7 +3,7 @@
 def call(String data_nombre = 'not_defined') {
 
 	def repo_ruta = pwd() + File.separator + data_nombre
-	def database = repo_ruta + File.separator + "bbdd_info.yaml"
+	def database = repo_ruta + File.separator "data"+ File.separator + "bbdd_info.yaml"
 
 	echo "leer el archivo ${database}"
 	def info = readYaml (file: "${database}")
