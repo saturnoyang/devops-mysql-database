62f9b45c4ac5e66f747ca46def7a246b8b40a3c6
Ricardo Sanchez <rjsmweb@gmail.com>
2022-04-04 16:02:54 -0400

    pruebas de integracion

--- vars/RunDatabaseScripts.groovy
+++ vars/RunDatabaseScripts.groovy
@@ -7,7 +7,7 @@ def call(java.util.LinkedHashMap db_details, String database, String repo_nombre
 	echo db_details."puerto"
 	echo db_details."usuario"
 	echo db_details."password"
-	mysql = "mysql -h " +db_details."url" + " -P "+db_details."puerto"+" -u "+db_details."usuario"+" ${database} -e \""
+	mysql = "mysql -h " +db_details."url" + " -P "+db_details."puerto"+" -u "+db_details."usuario"+" ${database} -e " + '"'
 
 	echo mysql
 
@@ -16,7 +16,7 @@ def call(java.util.LinkedHashMap db_details, String database, String repo_nombre
 
 
 	new File(repo_ruta).traverse(type: FileType.FILES,nameFilter   : ~/.*\.sql/) {
-    	mysql + it + "\""
+    	mysql + it + '"'
 	}
 
 
